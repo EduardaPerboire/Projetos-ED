2dc1ad975ec6358148812a8ecb0a0454ef66f50a
diegomendes1 <diegomendes1998@gmail.com>
2018-03-24 13:28:31 -0300

    Finalizado, em busca de bugs
    Não encontramos nenhum bug, mas é bom continuar procurando enquanto temos tempo.

--- src/principal/Conversao.java
+++ src/principal/Conversao.java
@@ -5,42 +5,52 @@ public class Conversao {
 	public int converterBaseParaDecimal(String digitos, int b) {
 		if(digitos.length() != 8) {
 			System.out.println("--- ERRO: A STRING PRECISA TER 32 BITS ---");
-			return -51;
+			return -000;
 		}else if(b < 2 || b > 16) {
 			//Fora de alcance
 			System.out.println("--- ERRO: A BASE PRECISA SER ENTRE 2 E 16 --- ");
-			return -51;
+			return -000;
 		}else if(b == 10) {
 			return Integer.parseInt(digitos);
+		}else {
+			for(int i = 0; i < digitos.length(); i++) {
+				if(transformarCharParaInt(digitos.charAt(i)) >= b) {
+					System.out.println("--- ERRO: DIGITOS DE BASE DIFERENTE DA ESPECIFICADA ---");
+					return -000;
+				}
+			}
 		}
 		
-		
 		digitos = digitos.toUpperCase();
+		boolean isNegativo = false;
 		int tamanho = digitos.length();
 		
-		int a = 0, n = 0;
 		if(transformarCharParaInt(digitos.charAt(0)) == b-1) {
-				String temp = "";
-				for(int i = 0; i < digitos.length(); i++) {
-					n = transformarCharParaInt(digitos.charAt(i));
-					a = (b - 1) - (n);
+			int digitoConvertido = 0, digitoAtual = 0;
+			isNegativo = true;
+			String temp = "";
+			
+			for(int i = 0; i < digitos.length(); i++) {
+				digitoAtual = transformarCharParaInt(digitos.charAt(i));
+				digitoConvertido = (b - 1) - (digitoAtual);
 					
-					if(i == digitos.length()-1) {
-						a++;
-					}
-					temp += transformarIntParaChar(a);
+				if(i == digitos.length()-1) {
+					digitoConvertido++;
 				}
-				
-				for(int i = digitos.length()-1; i >= 0; i--) {
-					int atual = transformarCharParaInt(temp.charAt(i));
-					if(atual >= b) {
-						temp = temp.substring(0, i) + 0 + temp.substring(i+1);
-						temp = temp.substring(0, i-1) + (transformarCharParaInt(temp.charAt(i-1))+1) + temp.substring(i);
-					}
+				temp += transformarIntParaChar(digitoConvertido);
+			}
+			
+			for(int i = digitos.length()-1; i >= 0; i--) {
+				int atual = transformarCharParaInt(temp.charAt(i));
+				if(atual >= b) {
+					temp = temp.substring(0, i) + 0 + temp.substring(i+1);
+					
+					temp = temp.substring(0, i-1)
+							+ (transformarCharParaInt(temp.charAt(i-1))+1)
+							+ temp.substring(i);
 				}
-				System.out.println(temp);
-				//digitos = temp;
-				
+			}
+			digitos = temp;
 		}
 		
 		int resultado = 0;
@@ -52,12 +62,77 @@ public class Conversao {
 			resultado += atual * multiplicador;
 			
 		}
-
-		System.out.println(digitos);
+		
+		if(isNegativo) {
+			resultado = resultado * -1;
+		}
+		return resultado;
+	}
+	
+	public String converterDecimalParaBase(int valor, int b) {
+		if(b < 2 || b > 16) {
+			//Fora de alcance
+			return "--- ERRO: A BASE PRECISA SER ENTRE 2 E 16 --- ";
+		}else if(b == 10){
+			return "" + valor;
+		}
+		
+		boolean isNegativo = false;
+		if(valor < 0) {
+			isNegativo = true;
+			valor = valor * -1;
+		}
+		
+		String resultado = "";
+		if(valor < b) {
+			for(int index = 0; index < 32; index++) {
+				if(isNegativo) {
+					resultado += transformarIntParaChar(b-1);
+				}else {
+					resultado += "0";
+				}
+			}
+			
+			if(isNegativo) {
+				resultado += transformarIntParaChar(b-valor);
+			}else {
+				resultado += transformarIntParaChar(valor);
+			}
+			
+			resultado = resultado.substring(1);
+			return resultado;
+		}
+		resultado += converterDecimalParaBase(valor/b, b) 
+				   + transformarIntParaChar(valor % b);
+		resultado = resultado.substring(1);
+		
+		if(isNegativo) {
+			int digitoConvertido = 0, digitoAtual = 0;
+			String temp = "";
+			for(int i = 0; i < resultado.length(); i++) {
+				digitoAtual = transformarCharParaInt(resultado.charAt(i));
+				digitoConvertido = (b - 1) - (digitoAtual);
+				
+				if(i == resultado.length()-1) {
+					digitoConvertido++;
+				}
+				temp += transformarIntParaChar(digitoConvertido);
+			}
+			
+			for(int i = resultado.length()-1; i >= 0; i--) {
+				int atual = transformarCharParaInt(temp.charAt(i));
+				if(atual >= b) {
+					temp = temp.substring(0, i) + 0 + temp.substring(i+1);
+					temp = temp.substring(0, i-1) + (transformarCharParaInt(temp.charAt(i-1))+1) + temp.substring(i);
+				}
+			}
+			resultado = temp;
+		}
 		return resultado;
 	}
 	
 	public int transformarCharParaInt(char c) {
+	
 		int numero = 0;
 		
 		switch(c) {
@@ -85,58 +160,6 @@ public class Conversao {
 		}
 		
 		return numero;
-	}
-	
-	public String converterDecimalParaBase(int valor, int b) {
-		if(b < 2 || b > 16) {
-			//Fora de alcance
-			return "--- ERRO: A BASE PRECISA SER ENTRE 2 E 16 --- ";
-		}
-		
-		boolean isNegativo = false;
-		if(valor < 0) {
-			isNegativo = true;
-			valor = valor * -1;
-		}
-		
-		String resultado = "";
-		if(valor < b) {
-			int i = valor % b;
-			for(int index = 0; index < 32; index++) {
-				resultado += "0";
-			}
-			resultado = resultado.substring(1);
-			resultado += transformarIntParaChar(i);
-			return resultado;
-		}
-		
-		resultado += converterDecimalParaBase(valor/b, b);
-		resultado += transformarIntParaChar(valor % b);
-		resultado = resultado.substring(1);
-		
-		int a = 0, n = 0;
-		if(isNegativo) {
-			String temp = "";
-			for(int i = 0; i < resultado.length(); i++) {
-				n = transformarCharParaInt(resultado.charAt(i));
-				a = (b - 1) - (n);
-				
-				if(i == resultado.length()-1) {
-					a++;
-				}
-				temp += transformarIntParaChar(a);
-			}
-			
-			for(int i = resultado.length()-1; i >= 0; i--) {
-				int atual = transformarCharParaInt(temp.charAt(i));
-				if(atual >= b) {
-					temp = temp.substring(0, i) + 0 + temp.substring(i+1);
-					temp = temp.substring(0, i-1) + (transformarCharParaInt(temp.charAt(i-1))+1) + temp.substring(i);
-				}
-			}
-			resultado = temp;
-		}
-		return resultado;
 	}
 	
 	public String transformarIntParaChar(int numero) {
--- src/principal/Main.java
+++ src/principal/Main.java
@@ -6,10 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 		Conversao conversao = new Conversao();
 		int numero = conversao.converterBaseParaDecimal("00110011", 2);
-		System.out.println(numero);
+		System.out.println("Resultado em decimal: " + numero);
 		
-		//String numero = conversao.converterDecimalParaBase(51, 16);
-		//System.out.println(numero);
+		//String numero = conversao.converterDecimalParaBase(51, 2);
+		//System.out.println("Resultado: " + numero);
 	}
 
 }
